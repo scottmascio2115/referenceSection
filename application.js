b08f3d062184562d5beee6f519b44ffa2e30e0ad
Scott Mascio <scottmascio@gmail.com>
2014-04-17 17:58:02 -0400

    iteration 1

---
+++ application.js
@@ -0,0 +1,41 @@
+(function($){
+  $.fn.reference = function() {
+    var i = 1
+    $( "body" ).append("<hr><h3 id='reference'>Reference Section</h3>");
+    $("#reference").css({"text-align": "center", "color": "Green"});
+    this.filter( "a" ).each(function() {
+      var link = $( this );
+      var href = link.attr("href");
+      var info = $( this).data('info');
+      link.after(" "+ "<a href="+"#"+i+">" + "<sup>" + "[" + i + "]"+ "</sup>" + "</a>");
+      if ( info !== undefined)
+        {
+      $( "body" ).append("<li id="+i+">"+"<sup>"+"[" + i + "]"+"</sup>"+ " "+href+ ":" + " " +info+ "</li>");
+        }
+        else
+          {
+      $( "body" ).append("<li id="+i+">"+"<sup>"+"[" + i + "]"+"</sup>"+ " "+href+ ":" + " " + "Description Not Found" + "</li>");
+          }
+
+      $("li").css("list-style-type", "none");
+      ++i;
+    });
+
+    $("sup").hover(
+      function() {
+      var parents_sibling = $( this).parent().prev('a');
+      var data = parents_sibling.data('info');
+      if ( data !== undefined)
+        {
+          $( this ).append( $( "<span>" +data+ "</span>" ) );
+        }
+    },function() {
+      $( this ).find( "span:last" ).remove();
+    });
+
+    return this;
+  };}(jQuery));
+
+  $( document ).ready(function() {
+    $( "a" ).reference();
+  });
